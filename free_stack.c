ea9d3bb526da32b3e2a23a2d3b797e5ae602c990
Amarah04 <Amarahorji@gmail.com>
2023-10-19 11:59:27 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

---
+++ free_stack.c
@@ -0,0 +1,17 @@
+#include "func.h"
+/**
+* free_stack - free doubly linked list
+* @head:the  head of the stack
+*/
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "func.h"
+/**
+ * f_stack - print top
+ * @head: stack head
+ * @counter: line_number
+ * Return: none
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
